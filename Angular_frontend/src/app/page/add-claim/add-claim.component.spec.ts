2d4b1359c60dc1d558d85f1f863edcab0e3644d9
Khouloud TAOUCHIKHT <92301300+taouchikhtkhouloud@users.noreply.github.com>
2023-01-22 00:30:37 +0100

    addclaim

---
+++ Angular_frontend/src/app/page/add-claim/add-claim.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddClaimComponent } from './add-claim.component';
+
+describe('AddClaimComponent', () => {
+  let component: AddClaimComponent;
+  let fixture: ComponentFixture<AddClaimComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AddClaimComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AddClaimComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
