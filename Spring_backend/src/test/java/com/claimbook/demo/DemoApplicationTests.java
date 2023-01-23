a8a7403800e940ff1c8ae504fcca9d48f0c914c0
Oussama Louati <louati.oussama@ine.inpt.ma>
2023-01-23 01:34:23 +0100

    starting the backend project

---
+++ Spring_backend/src/test/java/com/claimbook/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claimbook.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
