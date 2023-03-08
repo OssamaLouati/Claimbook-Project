bcce022f819d155c408e1ace9bf0aa7d7630b25e
Khouloud TAOUCHIKHT <92301300+taouchikhtkhouloud@users.noreply.github.com>
2023-03-08 10:16:23 +0100

    logout button

--- Angular_frontend/src/app/service/loginuser.service.ts
+++ Angular_frontend/src/app/service/loginuser.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 import { User } from '../user';
 
@@ -8,12 +9,26 @@ import { User } from '../user';
 })
 export class LoginuserService {
 
+  private userSubject = new BehaviorSubject<any>(null);
+  public user$ = this.userSubject.asObservable();
+
   constructor(private httpClient: HttpClient) { 
   
   }
-  loginUser(email: string ,password: string ){
+  loginUser(email: string ,password: string ): Observable<any>{
    
-    return this.httpClient.get('http://localhost:8082/user/'+email+'/'+password);
+    return this.httpClient.get<any>('http://localhost:8082/user/'+email+'/'+password);
       
   }
+  logout(): Observable<any> {
+    return this.httpClient.post<any>("http://localhost:8082/user/logout", {});
+  }
+  public setUser(user: any) {
+    this.userSubject.next(user);
+  }
+
+  public clearUser() {
+    this.userSubject.next(null);
+  }
+  
 }
--- Spring_backend/src/main/java/com/backend/springbackend/UserService.java
+++ Spring_backend/src/main/java/com/backend/springbackend/UserService.java
@@ -1,10 +1,13 @@
 package com.backend.springbackend;
 
+import java.sql.SQLException;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserService {
 	
 	public int loginValidation(String email , String password);
+	public User finduser(String email , String password) throws SQLException;
 
 }
